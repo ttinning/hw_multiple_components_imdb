1e3786149a6135df960f00869355e9220cfd6a5b
Tristan Tinning <tristan_tinning@hotmail.com>
2021-06-21 16:04:32 +0100

    MVP and form extension complete

---
+++ src/components/MoreRelease.js
@@ -0,0 +1,11 @@
+const MoreReleaseButton  = () => {
+    return(
+        <>
+            <form action="https://www.imdb.com/calendar/?region=gb">
+                <button type="submit">View More Releases</button>
+            </form>
+        </>
+    )
+}
+
+export default MoreReleaseButton
--- src/containers/FilmBox.js
+++ src/containers/FilmBox.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import FilmList from "../components/FilmList";
+import MoreReleaseButton from "../components/MoreRelease";
 import FilmForm from "./FilmForm";
 
 const FilmBox = () => {
@@ -14,22 +15,37 @@ const FilmBox = () => {
         [
             {
                 id: 1,
-                title: "Fast & Furious 9",
-                url: "https://www.imdb.com/title/tt5433138/?ref_=rlm"
+                title: "Spider-Man: Into the Spider-Verse",
+                url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
             },
             {
                 id: 2,
-                title: "Supernova",
-                url: "https://www.imdb.com/title/tt11169050/?ref_=rlm"
+                title: "Life Itself",
+                url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+            },
+            {
+                id: 3,
+                title: "Mary Queen of Scots",
+                url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+            },
+            {
+                id: 4,
+                title: "The Lego Movie 2: The Second Part", url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+            },
+            {
+                id: 5,
+                title: "Captain Marvel",
+                url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
             }
         ]
     )
 
     return(
         <>
-        <h1>Film Releases</h1>
-        <FilmList films={films}/>
-        <FilmForm onFilmSubmit={(film) => {addFilm(film)}}/>
+            <h1>Film Releases</h1>
+            <FilmList films={films}/>
+            <MoreReleaseButton/>
+            <FilmForm onFilmSubmit={(film) => {addFilm(film)}}/>
         </>
     )
 }
